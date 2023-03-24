b3d54d75a0bdb331cabe17561dcd8e055281c16b
Ryan <125604520+dowdryan@users.noreply.github.com>
2023-03-24 17:49:35 -0400

    Update Movies App.js

--- Unit 16/Movies App.js
+++ Unit 16/Movies App.js
@@ -1,15 +1,8 @@
-let numOfMovies = 0
-let moviesList = []
-
-let mylist = $('#ul')
-let listitems = mylist.children('li').get();
-
 $(function() {
     $("#form").on("submit", function(event) {
         event.preventDefault()
         let title = $("#title").val();
         let rating = $("#rating").val();
-        numOfMovies++
         $('#ul').append(`<li>${title}: ${rating}/10</li>`)
         $('#delete').css("visibility", "visible");
     })
