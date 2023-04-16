6ca933b06c2f35aa12a937a223b5275d1df55e27
Ryan <125604520+dowdryan@users.noreply.github.com>
2023-04-16 16:33:12 -0400

    Update serial.py

--- Unit 21/21.4/serial.py
+++ Unit 21/21.4/serial.py
@@ -1,24 +1,6 @@
 """Python serial number generator."""
 
 class SerialGenerator:
-    """Machine to create unique incrementing serial numbers.
-    
-    >>> serial = SerialGenerator(start=100)
-
-    >>> serial.generate()
-    100
-
-    >>> serial.generate()
-    101
-
-    >>> serial.generate()
-    102
-
-    >>> serial.reset()
-
-    >>> serial.generate()
-    100
-    """
     def __init__(self, start):
         """Initialize the Serial Number Generator"""
         self.start = self.next = start
